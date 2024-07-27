ffd00691910b3a8d74b4b52c167f85148f47ecff
yue hong <cses20130312@gamil.com>
2024-07-28 02:11:52 +0800

    預約查詢修改+優化

--- pages/ReservationGet/index.js
+++ pages/ReservationGet/index.js
@@ -21,6 +21,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 import ReservationEdit from '../ReservationEdit'
 import ReservationPay from '../ReservationPay'
+import toast, { Toaster } from 'react-hot-toast'
 
 const { Column, HeaderCell, Cell } = Table
 
@@ -32,15 +33,13 @@ export default function ReserveGet() {
   const [selectedReservation, setSelectedReservation] = useState(null)
   const [expandedRowKeys, setExpandedRowKeys] = useState([])
   const [memberId, setMemberId] = useState('')
-  // const [balance, setBalance] = useState(0)
 
   useEffect(() => {
-    // 在客戶端獲取localStorage
     const memberId = localStorage.getItem('memberId')
     if (memberId) {
       setMemberId(memberId)
     }
-  }, []) // 只在組件mount時執行一次
+  }, [])
   useEffect(() => {
     fetchReservations()
   }, [memberId])
@@ -52,7 +51,7 @@ export default function ReserveGet() {
       )
       const reservationsWithSteps = response.data.map((reservation) => ({
         ...reservation,
-        currentStep: reservation.status, // 使用從後端獲取的status來設定currentStep
+        currentStep: reservation.status, 
       }))
       setReservations(reservationsWithSteps)
       setLoading(false)
@@ -62,33 +61,17 @@ export default function ReserveGet() {
     }
   }
 
-  // const fetchBalance = async () => {
-  //   try {
-  //     if (!memberId) return // Exit early if memberId is not available
-  //     const response = await axios.get(
-  //       `http://localhost:3001/reserve/SaveMoneySystem/balance/${memberId}`
-  //     )
-  //     setBalance(response.data.balance)
-  //   } catch (error) {
-  //     console.error('Error fetching balance:', error)
-  //   }
-  // }
-
   const deleteReservation = async (id) => {
     try {
       const response = await axios.delete(
         `http://localhost:3001/reserve/reservationDelete/${id}`
       )
       if (response.data) {
-        // console.log(123)
-        // setReservations(response.data)
-        //測試過程中發現刪除主要有2問題
-        //1.刪除後畫面沒反應 重整後才會更新 最後使用setReservations(response.data)解決這樣她會帶入新的資料來渲染
-        //2.刪除後step全部回歸1 但我已經將step變成資料表儲存不該有這樣狀況 檢查後發現重整一樣會恢復 最後使用fetchReservations()達成刪除後重新搜尋
-        //3.最後我發現setReservations(response.data)可以扔掉了 因為他跟fetchReservations()同概念 但是fetchReservations()會將最新狀態帶回來 而不是針對資料filter而已
         fetchReservations()
+        toast.success('刪除成功')
       } else {
         console.error('Error: Did not receive expected response data.')
+        toast.error('刪除失敗!!')
       }
     } catch (error) {
       console.error('Error deleting reservation:', error)
@@ -153,7 +136,11 @@ export default function ReserveGet() {
       <Panel className={`${styles.renderRowPanel}`}>
         <Steps
           current={rowData.currentStep}
-          style={{ marginBottom: '20px', maxWidth: '1024px', minWidth: '80vw' }}
+          style={{
+            marginBottom: '20px',
+            maxWidth: '1024px',
+            minWidth: '1024px',
+          }}
         >
           <Steps.Item title="已完成預約" />
           <Steps.Item title="尚未支付訂金" />
@@ -175,12 +162,13 @@ export default function ReserveGet() {
       {loading && <Loader center content="我在努力中 > <" size="lg" backdrop />}
       {!loading && selectedReservationId === null && (
         <>
-          <div className="reservationPage">
+          <div className={`${styles.getTableList}`}>
             <Heading level={3} style={{ marginBlock: '15px' }}>
               尚未完成的預約
             </Heading>
             <Table
-              height={400}
+              height={550}
+              width={1280}
               data={reservations}
               rowKey="id"
               expandedRowKeys={expandedRowKeys}
@@ -229,7 +217,7 @@ export default function ReserveGet() {
                 <HeaderCell>桌型</HeaderCell>
                 <Cell dataKey="selectedTableType" />
               </Column>
-              <Column width={200}>
+              <Column width={150}>
                 <HeaderCell>您的備註</HeaderCell>
                 <Cell dataKey="textAreaInput" />
               </Column>
@@ -279,21 +267,14 @@ export default function ReserveGet() {
                 </Cell>
               </Column>
             </Table>
+            <Toaster />
           </div>
-          <style jsx>{`
-            .reservationPage {
-              margin: 0 auto;
-              min-height: 60.2vh;
-              width: 80%;
-            }
-          `}</style>
         </>
       )}
       {selectedReservationId !== null && (
         <ReservationEdit
           id={selectedReservationId}
           setSelectedReservationId={setSelectedReservationId}
-          //傳遞函式給ReservationEdit元件
           fetchReservations={fetchReservations}
         />
       )}
