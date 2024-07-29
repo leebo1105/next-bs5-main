6c8d2e69fb33d66dff2e53dbf35316344fc120c6
yue hong <cses20130312@gamil.com>
2024-07-29 19:54:06 +0800

    footer增加儲值金系統按鈕

--- pages/chatbotButton/index.js
+++ pages/chatbotButton/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 import styles from './chatbotButton.module.css'
+import Link from 'next/link'
 import ChatbotClient from '../ChatbotClient'
 
 export default function ChatbotButton() {
   const [button, setButton] = useState(false)
+  const router = useRouter()
 
   const handleButton = () => {
     if (button == false) {
@@ -12,11 +15,20 @@ export default function ChatbotButton() {
       setButton(false)
     }
   }
+  const handleSaveMoneyButtonClick = () => {
+    router.push('/SaveMoneySystem')
+  }
 
   return (
     <div className={`${styles.indexArea}`}>
       <button className={`${styles.chatbotButton}`} onClick={handleButton}>
         跟客服聯絡
+      </button>
+      <button
+        className={`${styles.saveMoneyButton}`}
+        onClick={handleSaveMoneyButtonClick}
+      >
+        我要儲值
       </button>
 
       {button && <ChatbotClient />}
