9a25a5d5623ce5820ee646fbf47093e52a040f64
yue hong <cses20130312@gamil.com>
2024-08-01 14:37:59 +0800

    3D更新

--- pages/ReserveCarousel/index.js
+++ pages/ReserveCarousel/index.js
@@ -26,7 +26,7 @@ export default function ReserveCarousel() {
         <iframe
           src="https://my.spline.design/untitled-af942951130cce33e9c0f72f659506bb/"
           frameborder="0"
-          width="80%"
+          width="100%"
           height="100%"
           title="牡丹樓的3D動畫"
         ></iframe>
